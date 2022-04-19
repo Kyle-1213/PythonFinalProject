d462aa7584c4063ef2a2d62febded53b1f3b40b3
Kyle-1213 <ledo1213@vandals.uidaho.edu>
2022-04-19 15:58:21 -0700

    4/19

--- classExecutable.py
+++ classExecutable.py
@@ -14,7 +14,7 @@ class Executable:
         self.__executableFile = open(fileName, 'r')
         self.__executableFileContents = self.__executableFile.readlines()
         self.generateSections()
-        self.test()
+        #self.test()
     def generateSections(self):
         for line in self.__executableFileContents:
             if "Disassembly of section" in line:
@@ -24,20 +24,22 @@ class Executable:
     def generateSectionContents(self, sectionName):
         sectionContents = ""
         readOn = False
-        #print(sectionName)
         for line in self.__executableFileContents:
             if sectionName in line and "Disassembly of section" in line:
                 readOn = True
-            elif(("Disassembly of section" not in line) and readOn == True):
+            elif(("Disassembly of section" not in line) and readOn == True and line != "\n" and line != ''):
                     sectionContents += line
             elif(("Disassembly of section" in line and sectionName not in line)):
                 readOn = False
         Section(sectionName, sectionContents)
     def printFileContents(self):
         print(self.__executableFileContents)
+    def getFile(self):
+        return self.__executableFileContents
     def test(self):
-        print("yes")
-
+        for a in Section.getSections():
+            a.printSectionName()
+            #a.printSectionContents()
 class Section():
     sectionList = []
     @staticmethod
@@ -49,11 +51,37 @@ class Section():
     def __init__(self, sectionName, sectionContents):
         Section.sectionList = Section.getSections()
         Section.sectionList.append(self)
-        self.sectionList.append(sectionName)
+        self.__sectionName = sectionName
         self.__sectionContents = sectionContents
-
+        self.generateFunction()
+    def generateFunction(self):
+        sectionSeparator = self.__sectionContents.split("\n")
+        for line in sectionSeparator:
+            if line != '\n' and line != '':
+                if line[0].isdigit() == True and line[-1] == ':':
+                    funtionName = line
+                    #print(line)
+                    SectionFunctions(self.__sectionName, self.__sectionContents)
+    def getSectionContents(self):
+        return self.__sectionContents
+    def getSectionName(self):
+        return self.__sectionName
+class SectionFunctions():
+    functionList = []
+    @staticmethod
+    def getFunctions():
+        myList = []
+        for x in SectionFunction.functionList:
+            myList.append(x)
+        return myList
+    def __init__(self, sectionName, sectionContents):
+        x=1
 def main():
     myExecutable = Executable("testDump.txt")
     #myExecutable.printFileContents()
+    for a in Section.getSections():
+        #print(a.getSectionName())
+        if ".text" in a.getSectionName():
+           print(a.getSectionContents())
 if __name__=="__main__":
     main()
